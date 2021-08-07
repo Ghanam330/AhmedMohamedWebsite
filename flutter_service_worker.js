729b3bb74fdde7731c0ee67249e45509d6c78a2c
AhmedGhanam <yghnam334455@gmail.com>
2021-08-07 03:16:54 +0200

    update1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -53,9 +53,9 @@ const RESOURCES = {
 "favicon.png": "61b493df9635f221d909421e8769acb0",
 "icons/Icon-192.png": "002516a4b23f153b7ac2928347c377b1",
 "icons/Icon-512.png": "84780d2dfe97536ad1b68213079cc78b",
-"index.html": "c081295a8203c3c5accf501c3ca2a115",
-"/": "c081295a8203c3c5accf501c3ca2a115",
-"main.dart.js": "d2bedd986c04770f2ffbabae481efe1a",
+"index.html": "8aa26fac6b974aad51084924b4249e47",
+"/": "8aa26fac6b974aad51084924b4249e47",
+"main.dart.js": "a98f55e9c1748d706b3da45785205a6b",
 "manifest.json": "855f8f0a3619e8d27e7210678871a4b0",
 "version.json": "1cefaf2a48f42633ff4b37ffe0e9de96"
 };
